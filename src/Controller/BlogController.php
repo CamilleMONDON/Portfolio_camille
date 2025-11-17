28b46e2624ef577f25c804773544dedba486a2cb
Camcamdelafolie <camcam.delafolie@gmail.com>
2025-11-17 08:21:19 +0100

    vertion anglais + lien entres pages

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -19,4 +19,25 @@ final class BlogController extends AbstractController
     {
         return $this->render('blog/cv.html.twig');
     }
+    #[Route('/portfolio', name: 'app_portfolio')]
+    public function portfolio(): Response
+    {
+        return $this->render('blog/portfolio.html.twig');
+    }
+    #[Route('/home/en', name: 'app_home_en')]
+    public function home_anglais(): Response
+    {
+        return $this->render('blog/en/home_en.html.twig');
+    }
+
+    #[Route('/cv/en', name: 'app_cv_en')]
+    public function CV_anglais(): Response
+    {
+        return $this->render('blog/en/cv_en.html.twig');
+    }
+    #[Route('/portfolio/en', name: 'app_portfolio_en')]
+    public function portfolio_anglais(): Response
+    {
+        return $this->render('blog/en/portfolio_en.html.twig');
+    }
 }
